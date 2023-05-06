ba77e11e0b284210ec919cdc1d3cd859d3168ecb
vinicius-celeghini <vinicius.celeghini@gmail.com>
2023-05-06 14:38:05 -0300

    importacao inicial

---
+++ java.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true,
+    },
+});
